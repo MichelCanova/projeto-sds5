e0e0b7e9507f0cf8c5d3a7ca1fb9473bd8434723
Michel Canova <michelcanova1@gmail.com>
2021-11-03 15:01:13 -0400

    update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,10 +11,10 @@ function App() {
         <h1 className="text-primary py-3">Dashboard de Vendas</h1>
         <div className="row px-3">
           <div className="col-sm-6">
-            <h5 className="text-center text-secondary">Taxa de Sucesso(%)</h5>
+            <h5 className="text-center text-secondary">Taxa de sucesso(%)</h5>
             <BarChart />
           </div><div className="col-sm-6">
-            <h5 className="text-center text-secondary">Taxa de Sucesso(%)</h5>
+            <h5 className="text-center text-secondary">Todas vendas(%)</h5>
             <DonutChart />
           </div>
         </div>
