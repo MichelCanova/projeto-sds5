6eb7bd1729b0bccc7be9e0e22864e4a3be927359
Michel Canova <michelcanova1@gmail.com>
2021-11-08 10:34:24 -0400

    Home, navigation

--- frontend/src/Pages/Home/index.tsx
+++ frontend/src/Pages/Home/index.tsx
@@ -1,8 +1,22 @@
-function Home(){
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+import { Link } from "react-router-dom";
+
+function Home() {
     return (
-        <div>
-           <h1>pagina Home</h1>.
-        </div>
+        <>
+            <NavBar />
+            <div className="container">
+                <div className="jumbotron">
+                    <h1 className="display-4">DSVendas</h1>
+                    <p className="lead">Analise de desempenho das suas vendas por diferentes perpectivas</p>
+                    <hr />
+                    <p>Este aplicativo consiste em exibir um dashboard apartir de dados fornecidos por um backend construido com spring boot.</p>
+                    <Link className="btn btn-primary btn-lg" to="/dashboard">Acessar o Dashboard</Link>
+                </div>
+            </div>
+            <Footer />
+        </>
     );
 }
 
